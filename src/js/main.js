1bedf4edef869e3cecbd20d6555adbb738cce105
Azat Irgalin <a.irgalin@napoleonit.ru>
2022-03-26 16:03:41 +0500

    Surf-01: adds footers styles and mobile nav menu

--- src/js/main.js
+++ src/js/main.js
@@ -2,5 +2,28 @@ import * as funcs from './modules/functions.js';
 
 funcs.isWebp();
 
-const count = num => num * 2;
-console.log(count(2));
+// Главное меню
+const mainMenu = document.querySelector('.nav__list');
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    mainMenu.classList.toggle('nav__list--active');
+    hamburger.classList.toggle('hamburger--active');
+});
+
+const menuLinks = document.querySelectorAll('.nav__link');
+
+for(let menuLink of menuLinks) {
+    menuLink.addEventListener('click', (elem) => {
+        
+        elem.preventDefault();
+        let scrollToElem = menuLink.getAttribute('href');
+        let coordinates = document.querySelector(scrollToElem).offsetTop;
+        window.scrollTo({
+            top: coordinates - 100,
+            behavior: 'smooth'
+        });
+        
+        mainMenu.classList.toggle('nav__list--active');
+    });
+}
