ebd7f11ff33ab1ef79159ef6e2b038a9bc0c4de1
Azat Irgalin <a.irgalin@napoleonit.ru>
2022-03-27 13:03:16 +0500

    Surf-01: adds popular goods tabs functionality

--- src/js/main.js
+++ src/js/main.js
@@ -27,3 +27,29 @@ for(let menuLink of menuLinks) {
         mainMenu.classList.toggle('nav__list--active');
     });
 }
+
+// Табы популярные товары
+const categoryBtns = document.querySelectorAll('.popular-goods__category-link');
+const categoryTabs = document.querySelectorAll('.popular-goods__tab');
+
+const removeActiveClasses = () => {
+    categoryBtns.forEach((btn) => {
+        btn.classList.remove('popular-goods__category-link--active');
+    });
+
+    categoryTabs.forEach((tab) => {
+        tab.classList.add('visually-hidden');
+    });
+};
+
+for (let i = 0; i < categoryBtns.length; i++) {
+    const btn = categoryBtns[i];
+    const tab = categoryTabs[i];
+
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        removeActiveClasses();
+        btn.classList.add('popular-goods__category-link--active');
+        tab.classList.remove('visually-hidden');
+    });
+}
