03c58c9797b9346200f7b81f7b75eea78c431c52
Azat Irgalin <a.irgalin@napoleonit.ru>
2022-04-02 11:44:34 +0500

    Surf-01: adds modal-map

--- src/js/main.js
+++ src/js/main.js
@@ -138,3 +138,37 @@ document.addEventListener('click', event => {
     }
 });
 
+// Модальное окно карта
+const modalMap = document.querySelector('.modal-map');
+const modalMapBtns = document.querySelectorAll('.btn--map');
+const mapBtnClose = document.querySelector('.modal-map__btn-close');
+
+modalMapBtns.forEach(modalMapBtn => {
+    modalMapBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        modalMap.classList.add('modal--active');
+        main.classList.add('visually-hidden');
+        header.classList.add('visually-hidden');
+        footer.classList.add('visually-hidden');
+    });
+});
+
+mapBtnClose.addEventListener('click', () => {
+    modalMap.classList.remove('modal--active');
+    main.classList.remove('visually-hidden');
+    header.classList.remove('visually-hidden');
+    footer.classList.remove('visually-hidden');
+});
+
+document.addEventListener('click', event => {
+    let target = event.target;
+    if (target.closest('.modal__wrap')) {
+        event.stopPropagation();
+    } else if (target.closest('.modal')) {
+        modalMap.classList.remove('modal--active');
+        main.classList.remove('visually-hidden');
+        header.classList.remove('visually-hidden');
+        footer.classList.remove('visually-hidden');
+    }
+});
+
