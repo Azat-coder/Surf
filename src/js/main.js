5a8225ae8c7b1ee2f0b78a76cbc5de16249e0f69
Azat Irgalin <a.irgalin@napoleonit.ru>
2022-04-02 10:36:09 +0500

    Surf-01: adds modal-quickview

--- src/js/main.js
+++ src/js/main.js
@@ -68,24 +68,73 @@ for (let i = 0; i < categoryBtns.length; i++) {
 
 // Модальное окно заявка
 const modal = document.querySelector('.modal-request');
-const modalBtn = document.querySelector('.btn--request');
-const btnClose = document.querySelector('.modal-request__btn-close');
-const nameInput = document.querySelector('#modal-request__name');
+const modalBtns = document.querySelectorAll('.btn--request');
+const btnClose = document.querySelector('.modal__btn-close');
+const focusInput = document.querySelector('#modal-request__name');
+const main = document.querySelector('.main');
+const header = document.querySelector('.header');
+const footer = document.querySelector('.footer');
 
-modalBtn.addEventListener('click', () => {
-    modal.classList.add('modal--active');
-    nameInput.focus();
+
+modalBtns.forEach(modalBtn => {
+    modalBtn.addEventListener('click', () => {
+        modal.classList.add('modal--active');
+        focusInput.focus();
+        main.classList.add('visually-hidden');
+        header.classList.add('visually-hidden');
+        footer.classList.add('visually-hidden');
+    });
 });
 
 btnClose.addEventListener('click', () => {
     modal.classList.remove('modal--active');
+    main.classList.remove('visually-hidden');
+    header.classList.remove('visually-hidden');
+    footer.classList.remove('visually-hidden');
 });
 
-document.addEventListener("click", (event) => {
+document.addEventListener('click', event => {
     let target = event.target;
     if (target.closest('.modal__wrap')) {
         event.stopPropagation();
     } else if (target.closest('.modal')) {
         modal.classList.remove('modal--active');
+        main.classList.remove('visually-hidden');
+        header.classList.remove('visually-hidden');
+        footer.classList.remove('visually-hidden');
     }
 });
+
+// Модальное окно быстрый товар
+const modalQuickView = document.querySelector('.modal-quickview');
+const modalQuickViewBtns = document.querySelectorAll('.btn--quickview');
+const quickViewBtnClose = document.querySelector('.modal-quickview__btn-close');
+
+modalQuickViewBtns.forEach(modalQuickViewBtn => {
+    modalQuickViewBtn.addEventListener('click', () => {
+        modalQuickView.classList.add('modal--active');
+        main.classList.add('visually-hidden');
+        header.classList.add('visually-hidden');
+        footer.classList.add('visually-hidden');
+    });
+});
+
+quickViewBtnClose.addEventListener('click', () => {
+    modalQuickView.classList.remove('modal--active');
+    main.classList.remove('visually-hidden');
+    header.classList.remove('visually-hidden');
+    footer.classList.remove('visually-hidden');
+});
+
+document.addEventListener('click', event => {
+    let target = event.target;
+    if (target.closest('.modal__wrap')) {
+        event.stopPropagation();
+    } else if (target.closest('.modal')) {
+        modalQuickView.classList.remove('modal--active');
+        main.classList.remove('visually-hidden');
+        header.classList.remove('visually-hidden');
+        footer.classList.remove('visually-hidden');
+    }
+});
+
