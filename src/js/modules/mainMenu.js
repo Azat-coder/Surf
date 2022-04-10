188786422756fd19918956bb98ac42f49c2244b1
Azat Irgalin <a.irgalin@napoleonit.ru>
2022-04-10 13:42:49 +0500

    Surf-01: fixes routing

--- src/js/modules/mainMenu.js
+++ src/js/modules/mainMenu.js
@@ -6,21 +6,4 @@ export function mainMenu() {
         mainMenu.classList.toggle('nav__list--active');
         hamburger.classList.toggle('hamburger--active');
     });
-
-    const menuLinks = document.querySelectorAll('.nav__link');
-
-    for(let menuLink of menuLinks) {
-        menuLink.addEventListener('click', (elem) => {
-        
-            elem.preventDefault();
-            let scrollToElem = menuLink.getAttribute('href');
-            let coordinates = document.querySelector(scrollToElem).offsetTop;
-            window.scrollTo({
-                top: coordinates - 100,
-                behavior: 'smooth'
-            });
-        
-            mainMenu.classList.toggle('nav__list--active');
-        });
-    }
 }
