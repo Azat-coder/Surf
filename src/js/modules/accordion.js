548c7b3dfef3be8b81e41c0e58e7bb4703a117e2
Azat Irgalin <a.irgalin@napoleonit.ru>
2022-05-14 16:55:21 +0500

    Surf-02: changes accordion logic and makes accordion titles unselectable

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -20,7 +20,20 @@ export function useAccordion (sectionClass, mobileWidth=false) {
             }
 
             accItem.addEventListener('click', () => {
-                if (!accItem.classList.contains('accordion--closed')) {
+                if (!accItem.classList.contains('accordion__item--closed')) {
+                    accContent.style.height = 0 + 'px';
+                    accContent.classList.add('accordion__content--hidden');
+                    accItem.classList.add('accordion__item--closed');
+                    const nextElem = acc.nextElementSibling;
+
+                    if (nextElem) {
+                        const nextElemContent = nextElem.querySelector('.accordion__content');
+                        const nextElemItem = nextElem.querySelector('.accordion__item');
+                        nextElemContent.style.height = nextElemContent.scrollHeight + 'px';
+                        nextElemContent.classList.remove('accordion__content--hidden');
+                        nextElemItem.classList.remove('accordion__item--closed');
+                    }
+                } else {
                     for (let index = 0; index < acc.parentElement.children.length; index++) {
                         const elem = acc.parentElement.children[index];
                         const siblingContent = elem.querySelector('.accordion__content');
@@ -29,14 +42,10 @@ export function useAccordion (sectionClass, mobileWidth=false) {
                         siblingContent.classList.add('accordion__content--hidden');
                         siblingItem.classList.add('accordion__item--closed');
                     }
-
+    
                     accContent.style.height = accContent.scrollHeight + 'px';
                     accContent.classList.remove('accordion__content--hidden');
                     accItem.classList.remove('accordion__item--closed');
-                } else {
-                    accContent.style.height = 0 + 'px';
-                    accContent.classList.add('accordion__content--hidden');
-                    accItem.classList.add('accordion__item--closed');
                 }
             });
         });
